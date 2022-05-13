38e31c5747dc1c51a2b0589d2ae1798a53330222
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-05-13 17:52:17 +0200

    added test

--- tests/factory_cache_tests.rs
+++ tests/factory_cache_tests.rs
@@ -39,3 +39,13 @@ fn confirm_cached() {
   cache.get(1);
   assert_eq!(COUNTER.count(), 1);
 }
+
+#[test]
+fn confirm_stable_cache() {
+  let factory_cache = FactoryCache::new(BTreeMap::new(), Box::new(|x: &i64| generate_square(x)));
+  let x: std::sync::Arc<i64> = factory_cache.get(42);
+  for i in 0..10 {
+    factory_cache.get(i);
+  }
+  assert_eq!(*x, generate_square(&42))
+}
