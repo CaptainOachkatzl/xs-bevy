586b626deac8e6bd4da263b41e5cf3c312ad327e
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-05-13 22:22:57 +0200

    test case

--- tests/factory_cache_tests.rs
+++ tests/factory_cache_tests.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, sync::{Barrier, Arc}, thread, time::Duration};
 
 use xs_bevy_core_2d::{Counter, FactoryCache};
 
@@ -48,4 +48,39 @@ fn confirm_stable_cache() {
     factory_cache.get(i);
   }
   assert_eq!(*x, generate_square(&42))
+}
+
+#[test]
+fn parallel_test() {
+  const THREAD_COUNT: usize = 100;
+
+  let factory_cache = Arc::new(FactoryCache::new(BTreeMap::new(), generate_square));
+
+  let barrier = Arc::new(Barrier::new(THREAD_COUNT));
+  let mut thread_handles = vec!();
+  for i in 0..THREAD_COUNT {
+    let thread_id = i;
+    let bar = Arc::clone(&barrier);
+    let factory = Arc::clone(&factory_cache);
+    thread_handles.push(thread::spawn(move || {
+      bar.wait();
+      execute_workload(thread_id, factory.as_ref());
+    }));
+  }
+
+  for handle in thread_handles {
+    handle.join().unwrap();
+  }
+}
+
+fn execute_workload(thread_id: usize, factory_cache: &FactoryCache<i64, i64, BTreeMap<i64, Arc<i64>>>) {
+  const WORKLOAD: i64 = 1000;
+  const BATCH_SIZE: i64 = 10;
+  
+  for i in 0..WORKLOAD {
+    let load_balanced = (i + thread_id as i64) % BATCH_SIZE;
+
+    factory_cache.get(load_balanced);
+    //generate_square(&load_balanced);
+  }
 }
