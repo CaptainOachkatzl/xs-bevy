c96aa1f77fe3d66d272df9d29d8078a3879d29de
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-05-16 18:42:43 +0200

    added rectangle pattern

--- src/patterns.rs
+++ src/patterns.rs
@@ -9,3 +9,6 @@ pub use grid_pattern::*;
 
 pub mod surrounding_pattern;
 pub use surrounding_pattern::*;
+
+pub mod rectangle_pattern;
+pub use rectangle_pattern::*;
--- src/patterns/surrounding_pattern.rs
+++ src/patterns/surrounding_pattern.rs
@@ -1,27 +1,11 @@
-use std::{
-  collections::BTreeMap,
-  sync::Arc, lazy::SyncLazy,
-};
-
-use crate::{index_translation::to_index, *};
+use std::sync::Arc;
 
 use super::*;
 
-pub fn surrounding_pattern(arm_length: usize) -> Arc<GridPattern> {
-  static mut PATTERN_CACHE: SyncLazy<FactoryCache<usize, GridPattern, BTreeMap<usize, Arc<GridPattern>>>> = SyncLazy::new(|| {
-    FactoryCache::new(BTreeMap::new(), Box::new(new_surrounding_pattern))
-  });
-
-  unsafe { PATTERN_CACHE.get(arm_length) }
+pub fn surrounding_pattern(radius: usize) -> Arc<GridPattern> {
+  return rectangle_pattern(radius, radius, radius, radius);
 }
 
 pub fn new_surrounding_pattern(radius: usize) -> GridPattern {
-  let center = Position::from((radius, radius));
-  let diameter = radius * 2 + 1;
-  let mut mapping = vec![true; diameter * diameter];
-  mapping[to_index(center, Size2D::new(diameter, diameter))] = false;
-  GridPattern {
-    mapping: Grid::new(diameter, diameter, mapping.into_boxed_slice()),
-    center,
-  }
+  return new_rectangle_pattern(radius, radius, radius, radius);
 }
