9f66bfb2664c3e1abd616ffc055f5750f10ddb67
CaptainOachkatzl <dave.hofer@hotmail.com>
2023-08-05 08:36:53 +0200

    test build fix

--- tests/todo_list_tests.rs
+++ tests/todo_list_tests.rs
@@ -2,7 +2,7 @@ use xs_bevy_core_2d::TodoList;
 
 #[test]
 fn todo_list_test() {
-    let mut list = TodoList::<i32>::new();
+    let mut list = TodoList::<i32>::default();
     assert_eq!(list.get_new().len(), 0);
 
     list.push(1);
